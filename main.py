ab11236f0df8b29fe75142836aba92146ad9a77f
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2021-03-29 17:01:21 +0200

    Added functionality to te Dataset class

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 
     sim = Simulation()
     expert = Expert()
-    dataset = Dataset(n_steps)
+    dataset = Dataset("test.npy", image_path="data/images/", filemode="w")
     sim.update_state()
     state = sim.get_state()
     #sim.step_to(*(state.item.pos-[0.2, 0])
@@ -17,16 +17,16 @@ def main():
     for _ in range(100):
         sim.step_simulation()
 
-    for _ in range(n_steps):
+    for i in range(n_steps):
         state = sim.get_state()
         poke = expert.calculate_poke2(state.item, state.goal)
         sim.step_robot_to(*poke)
-        #  dataset.add(state.image, poke, i)
+        dataset.add(state.image, poke, i)
 
         sim.step_simulation()
 
     sim.terminate()
-    dataset.save_pokes()
+   # dataset.save_pokes()
 
 
 if __name__ == '__main__':
--- utilities.py
+++ utilities.py
@@ -1,6 +1,9 @@
 from PIL import Image
 from dataclasses import dataclass
+from datetime import datetime
 import numpy as np
+import os
+from os import path
 
 @dataclass
 class Item:
@@ -29,18 +32,45 @@ class State:
 
 
 class Dataset:
-    def __init__(self, steps):
-        self.steps = steps
-        self.pokes = np.empty((steps, 2))
+    def __init__(self, file_name, image_path=None, filemode="x"):
+        
+        self.idx = 0
+        self.dataID = datetime.now().strftime("%d-%m_%H:%M:%S")
+        self.path_to_store_img = image_path
+
+        if image_path == None: 
+            self.path_to_store_img = "data/images/" + self.dataID + "/"
+        else: 
+            self.path_to_store_img = image_path
+
+
+        if file_name[-4:] == ".npy":
+            self.file_name = file_name
+        else: 
+            self.file_name = file_name + ".npy"
+
+        try: 
+            self.file = open(self.file_name, filemode)
+        except: 
+            raise Exception("Datafile was not created")
+        
+        try:
+            os.makedirs(self.path_to_store_img)
+        except:
+            print("Directory already exists")
+        
+
+    def __del__(self):
+        self.file.close()
 
     def add(self, image: Image, poke: np.ndarray, idx):
-        # TODO: handle dynamic filepath given as argument
-        image.save('data/images/img' + str(idx).zfill(4) + '.png')
-        self.pokes[idx] = poke
+        image_name = self.dataID + "_" + str(self.idx).zfill(4) + '.png'
+        image.save(self.path_to_store_img + image_name)
+        self.file.write(image_name + ", " + str(poke[0]) + ", " + str(poke[1]) + "\n") 
+        self.idx += 1
 
-    def save_pokes(self):
-        # TODO: handle dynamic filepath given as argument
-        np.save('data/pokes.npy', self.pokes)
+
+
 
 
 
