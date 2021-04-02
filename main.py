ef791adad405e9a038a6f462aefd0842cf31d1b1
Sina Pour Soltani <sinapoursoltani@Sinas-MacBook-Pro.local>
2021-04-02 12:27:12 +0200

    Update expert to work as a state machine.

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ def main(args=None):
     sim = Simulation()
     expert = Expert()
 
-    dataset = Dataset("data/test.npy", image_path="data/images/", filemode="w")
     dataset = Dataset(args.data_file_name, image_path=args.image_path,  filemode=args.file_mode)
     sim.update_state()
     state = sim.get_state()
@@ -35,18 +34,13 @@ def main(args=None):
     sim.set_robot_pose(-0.3, -0.15)
     for _ in range(100):
         sim.step()
-    
 
     for i in range(n_steps):
         state = sim.get_state()
 
-        poke = expert.calculate_poke2(state.item, state.goal)
-        dataset.add(state.image, poke, i)
-        sim.step_robot_to(*poke)
-
         tcp_pose = sim.robotArm.get_tcp_pose()
         sim.draw_coordinate_frame(*tcp_pose)
-        poke = expert.calculate_poke2(tcp_pose, state.item, state.goal)
+        poke = expert.calculate_move(tcp_pose, state.item, state.goal)
         print(tcp_pose[0], ' + ', poke)
         
         #dataset.add(state.image, poke, i)
