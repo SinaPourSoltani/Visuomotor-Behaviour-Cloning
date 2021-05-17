f9ed7706be7c4122fd62c771a61082a1f0e168f4
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2021-05-17 15:10:41 +0200

    Updated concat functions

--- utilities.py
+++ utilities.py
@@ -7,14 +7,14 @@ from os import path
 
 
 #https://note.nkmk.me/en/python-pillow-concat-images/
-def get_concat_h(im1, im2):
-    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+def get_concat_h_blank(im1, im2, color=(0, 0, 0)):
+    dst = Image.new('RGB', (im1.width + im2.width, max(im1.height, im2.height)), color)
     dst.paste(im1, (0, 0))
     dst.paste(im2, (im1.width, 0))
     return dst
 
-def get_concat_v(im1, im2):
-    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+def get_concat_v_blank(im1, im2, color=(0, 0, 0)):
+    dst = Image.new('RGB', (max(im1.width, im2.width), im1.height + im2.height), color)
     dst.paste(im1, (0, 0))
     dst.paste(im2, (0, im1.height))
     return dst
